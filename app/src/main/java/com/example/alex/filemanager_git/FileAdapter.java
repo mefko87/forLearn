8aa3e7a8258aba86a4ece610f4880f55f627a4d3
Alex Kryvoruchko <ogkryvoruchko@gmail.com>
2016-12-21 20:55:11 +0200

    Створити адаптор який буде відображати список файлів

--- app/src/main/java/com/example/alex/filemanager_git/FileAdapter.java
+++ app/src/main/java/com/example/alex/filemanager_git/FileAdapter.java
@@ -2,20 +2,30 @@ package com.example.alex.filemanager_git;
 
 
 import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 
 public class FileAdapter extends BaseAdapter {
-	private ArrayList<FileList> fileList;
+	private ArrayList<File> fileList;
 	private Activity activity;
 	final String LOG_TAG = "myLogs";
 	ViewHolder holder;
+
+	public FileAdapter(ArrayList<File> fileList, Activity activity) {
+		this.fileList = fileList;
+		this.activity = activity;
+	}
 
 	private class ViewHolder {
 		TextView mName;
@@ -46,19 +56,77 @@ public class FileAdapter extends BaseAdapter {
 
 		if (convertView == null) {
 			LayoutInflater inflater = LayoutInflater.from(activity.getApplicationContext());
-			convertView = inflater.inflate(R.layout.file_folder, null);
+			convertView = inflater.inflate(R.layout.adapter_view, null);
 			holder = new ViewHolder();
 			holder.mName = (TextView) convertView.findViewById(R.id.name);
 			holder.mSize = (TextView) convertView.findViewById(R.id.size);
-//			holder.mExtension =
-//			holder.mCreateDate =
+			holder.mExtension = (TextView) convertView.findViewById(R.id.ext);
+			holder.mCreateDate = (TextView) convertView.findViewById(R.id.date);
 			convertView.setTag(holder);
 
 		} else {
 			holder = (ViewHolder) convertView.getTag();
 		}
+
+		File item = fileList.get(position);
+		if (item.isDirectory()) {
+			holder.mName.setText(item.getName());
+			holder.mSize.setText("DIR");
+			holder.mExtension.setText(" ");
+			holder.mCreateDate.setText(setLastModifiedDate(item));
+
+		} else if (item.getName().equals("empty")) {
+			holder.mName.setText("Folder is emty");
+			holder.mSize.setText(" ");
+			holder.mExtension.setText(" ");
+			holder.mCreateDate.setText(" ");
+		} else {
+
+			holder.mName.setText(setFileName(item));
+			holder.mSize.setText(Integer.toString((int) item.length()));
+			holder.mExtension.setText(getFileExt(item));
+			holder.mCreateDate.setText(setLastModifiedDate(item));
+		}
 		return convertView;
 	}
 
+	public String getFileExt(File file) {
+		String fileName = file.getName();
+		return fileName.substring(fileName.lastIndexOf(".") + 1, fileName.length());
+	}
 
+
+	public String setFileName(File file) {
+		String name = file.getName();
+		return name.split("\\.", 5)[0];
+
+	}
+
+	public String setLastModifiedDate(File file) {
+
+		return new SimpleDateFormat("yyyy.MM.dd  HH:mm:ss ").format(new Date(file.lastModified())).toString();
+	}
+
+	@Override
+	public void notifyDataSetChanged() {
+		super.notifyDataSetChanged();
+		Collections.sort(fileList, new Comparator<File>() {
+			@Override
+			public int compare(File file, File file1) {
+				return file.getName().compareTo(file1.getName());
+			}
+
+		});
+		Collections.sort(fileList, new Comparator<File>() {
+			@Override
+			public int compare(File file, File file1) {
+				if (file.isDirectory() == file1.isDirectory())
+					return 0;
+				else if (file.isDirectory() && !file1.isDirectory())
+					return -1;
+				else
+					return 1;
+			}
+		});
+	}
 }
